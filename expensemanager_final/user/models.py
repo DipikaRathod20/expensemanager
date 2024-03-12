d96698ebcd587128353f1b6501fbf97ed9090028
DipikaRathod20 <rathoddipika889@gmail.com>
2024-03-12 09:02:41 +0530

    initialize django project and setup

---
+++ expensemanager_final/user/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    is_manager = models.BooleanField(default=False)
+    is_client = models.BooleanField(default=False)
+    class Meta:
+        db_table = 'user' 
+    
